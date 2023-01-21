b07190711b37ef37484a1323e185428b54a5eaa7
quynt <quynguyenthe000@gmail.com>
2023-01-21 17:38:16 +0700

    update dockerfile

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -85,13 +85,24 @@ WSGI_APPLICATION = 'resume_builder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DB_ENGINE', 'sqlite3') == 'sqlite3':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME', 'quynt'),
+            'USER': os.environ.get('DB_USER', 'quynt'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'secret'),
+            'HOST': os.environ.get('DB_HOST', "127.0.0.1"),
+            'PORT': os.environ.get('DB_PORT', '5432')
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
